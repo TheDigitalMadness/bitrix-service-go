6c007adaff42068d0fbe9051393a5c50c280d12d
TheDigitalMadness <zakhar.burov050209@yandex.ru>
2026-06-14 09:27:39 +0300

    config fixed

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,30 @@
 package config
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 type Config struct {
 	HttpPort                         int
 	BitrixBaseUrl                    string
 	BitrixClientHttpTimeoutInSeconds int
 }
 
-// Типа реализовал получение данных из конфига
+// https://b24-5ji7no.bitrix24.ru
+// 1
+// TopSecret
 func New() *Config {
 	return &Config{
-		HttpPort:                         4000,
-		BitrixBaseUrl:                    "https://b24-5ji7no.bitrix24.ru/rest/1/TopSecret",
-		BitrixClientHttpTimeoutInSeconds: 6,
+		HttpPort:      4000,
+		BitrixBaseUrl: fmt.Sprintf("%s/rest/%s/%s", os.Getenv("BITRIX_URL"), os.Getenv("BITRIX_USER_ID"), os.Getenv("BITRIX_WEBHOOK_SECRET")),
+		BitrixClientHttpTimeoutInSeconds: func() int {
+			val, err := strconv.ParseInt(os.Getenv("BITRIX_CLIENT_HTTP_TIMEOUT"), 10, 64)
+			if err != nil || val <= 0 {
+				val = 6
+			}
+			return int(val)
+		}(),
 	}
 }
