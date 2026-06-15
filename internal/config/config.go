b4404710feb74b6fbafc1ee325a275a1bfc0ffa9
TheDigitalMadness <zakhar.burov050209@yandex.ru>
2026-06-15 12:50:48 +0300

    Dockerfile added

--- internal/config/config.go
+++ internal/config/config.go
@@ -18,7 +18,13 @@ type Config struct {
 // TopSecret
 func New() *Config {
 	return &Config{
-		HttpPort:      4000,
+		HttpPort: func() int {
+			val, err := strconv.ParseInt(os.Getenv("PORT"), 10, 64)
+			if err != nil || val <= 0 {
+				val = 4000
+			}
+			return int(val)
+		}(),
 		BitrixBaseUrl: fmt.Sprintf("%s/rest/%s/%s", os.Getenv("BITRIX_URL"), os.Getenv("BITRIX_USER_ID"), os.Getenv("BITRIX_WEBHOOK_SECRET")),
 		BitrixClientHttpTimeoutInSeconds: func() int {
 			val, err := strconv.ParseInt(os.Getenv("BITRIX_CLIENT_HTTP_TIMEOUT"), 10, 64)
